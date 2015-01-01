e535f3e95af3989aafca692e15574cb2e9bb95a7
houchenxi <495838087@qq.com>
2015-01-01 16:48:34 +0800

    【在应用程序委托中增加详细的注释】
    之前写代码的时候由于不理解，写出了异常，现在用注释表明你比较清楚示例代码中关键标识符的引用目标，这样能够限制错误的出现。

--- GeneralApp/AppDelegate.m
+++ GeneralApp/AppDelegate.m
@@ -18,31 +18,43 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    // 创建根控制器实例，并且赋给AppDelegate的属性保存
-    self.switchViewController = [[SwitchViewController alloc]
-                                 initWithNibName:@"SwitchViewController" bundle:nil];
+    // 这段代码用默认窗口的边框属性构造生成一个新的窗口
+    // 从视图文件中加载出来视图（编辑器设计生成的）并在窗口中将控制该视图的控制器设置为《根控制器》（关联视图到窗口）
+    {
+        // 0. 初始化窗口属性，使之从系统取到控制设备整个窗口的句柄类型
+        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        
+        // 1. 用控制器加载视图
+        // 这段代码构造一个控制器，并且加载工程文件中的视图文件（SwitchViewController.xib这个nib文件）
+        self.switchViewController = [[SwitchViewController alloc]
+                                     initWithNibName:@"SwitchViewController" bundle:nil];
 
-    // 从SwitchViewController.xib中加载视图
-    UIView* switchView = self.switchViewController.view;
+        // 2. 控制器定位视图
+        // 这段代码将视图的左上角定位到状态栏的底边上
+        {
+            // 从SwitchViewController.xib中加载视图
+            UIView* switchView = self.switchViewController.view;
+        
+            // 修改属性，以免状态栏覆盖这个视图
+            CGRect switchViewFrame = switchView.frame;
+            // 使视图紧靠状态栏底部
+            switchViewFrame.origin.y += [UIApplication
+                                         sharedApplication].statusBarFrame.size.height;
+            // 设置根控制器控制的视图
+            switchView.frame = switchViewFrame;
+        }
+        
+        // 3. 设置控制器到window属性中
+        // 根控制器的关联视图设置完毕，设置到窗口属性的根控制器属性
+        self.window.rootViewController = self.switchViewController;
+        
+        // 4. （此设置比较次要）设置窗口背景色为白色
+        self.window.backgroundColor = [UIColor whiteColor];
+        
+    }
     
-    // 修改属性，以免状态栏覆盖这个视图
-    CGRect switchViewFrame = switchView.frame;
-    // 使视图紧靠状态栏底部
-    switchViewFrame.origin.y += [UIApplication
-                                 sharedApplication].statusBarFrame.size.height;
-    // 设置根控制器控制的视图
-    switchView.frame = switchViewFrame;
-    
-    // 根控制器的关联视图设置完毕，设置到窗口属性的根控制器属性
-    self.window.rootViewController = self.switchViewController;
-    
-    // 设置窗口背景色为白色
-    self.window.backgroundColor = [UIColor whiteColor];
-    
+    // 0. 用属性中的window属性所设置好的UIWindow对象替换老的默认UIWindow
     [self.window makeKeyAndVisible];
-    
     
     return YES;
 }
